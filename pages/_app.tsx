d0492b0a73ad43b5dd9ad37279e61b0114aaa741
Gustavo Pazeto <gustavopazeto95@gmail.com>
2022-03-20 23:02:12 -0300

    Fix: Importação das fontes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,9 @@ import "../styles/globals.scss";
 import "@fontsource/inter/300.css";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
 import "@fontsource/inter/900.css";
 
 import Navbar from "components/Navbar";
